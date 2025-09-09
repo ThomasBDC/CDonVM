87bc878f2405aa5cafc51f37c8df6e3028bb4c25
TBDC <sio.bureauducolombier@gmail.com>
2025-09-09 19:53:39 +0200

    Add project files.

---
+++ CDonVM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CDonVM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
